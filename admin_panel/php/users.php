edf81a80dee490c101e5664a7e358141958ba935
Samar Chauhan <d24ce154@charusat.edu.in>
2024-09-29 13:32:07 +0530

    Update users.php
    Added a column appointment status

--- admin_panel/php/users.php
+++ admin_panel/php/users.php
@@ -9,7 +9,6 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
 </head>
     
-
 <?php
 include '../templates/connect_db.php';
 include '../templates/sidebar_breadcrumb.php';
@@ -28,7 +27,15 @@ try {
     $total_users_query = $pdo->query("SELECT COUNT(*) FROM user_info WHERE user_type = 'Normal'");
     $total_users = $total_users_query->fetchColumn();
 
-    $stmt = $pdo->prepare("SELECT user_id, fullname, email FROM user_info WHERE user_type = 'Normal' LIMIT :start, :limit");
+    // Fetch user information along with appointment status, date, and therapist ID
+    $stmt = $pdo->prepare("
+        SELECT u.user_id, u.fullname, u.email, 
+               a.status, a.appointment_date, a.therapist_id 
+        FROM user_info u 
+        LEFT JOIN appointment a ON u.user_id = a.patient_id 
+        WHERE u.user_type = 'Normal' 
+        LIMIT :start, :limit");
+        
     $stmt->bindParam(':start', $start_from, PDO::PARAM_INT);
     $stmt->bindParam(':limit', $records_per_page, PDO::PARAM_INT);
     $stmt->execute();
@@ -55,6 +62,8 @@ try {
                     <th>User ID</th>
                     <th>Name</th>
                     <th>Email</th>
+                    <th>Appointment Status</th>
+                    <th>Appointment Details</th>
                     <th>Action</th>
                 </tr>
             </thead>
@@ -64,6 +73,15 @@ try {
                         <td><?php echo $user['user_id']; ?></td>
                         <td><?php echo htmlspecialchars($user['fullname']); ?></td>
                         <td><?php echo htmlspecialchars($user['email']); ?></td>
+                        <td><?php echo htmlspecialchars($user['status']); ?></td>
+                        <td>
+                            <?php if ($user['status'] === 'Scheduled'): ?>
+                                Date: <?php echo htmlspecialchars($user['appointment_date']); ?><br>
+                                Therapist ID: <?php echo htmlspecialchars($user['therapist_id']); ?>
+                            <?php else: ?>
+                                <?php echo htmlspecialchars($user['status']); ?>
+                            <?php endif; ?>
+                        </td>
                         <td><a href="edit_user.php?id=<?php echo $user['user_id']; ?>" class="edit-btn">Edit</a></td>
                     </tr>
                 <?php endforeach; ?>
@@ -73,7 +91,6 @@ try {
 
     <div class="pagination">
         <?php
-
         $total_pages = ceil($total_users / $records_per_page);
 
         if ($current_page > 1) {
